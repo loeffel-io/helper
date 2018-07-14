759aca5628bab73b2992c10bd7e51c46dc62f791
Lucas Löffel <lucas@loeffel.io>
2018-07-14 17:55:03 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # helper
+
+Simple helper package 
+
+### Sentry support
+
+This package supports [sentry.io](https://sentry.io) real time error reporting.
+More informations: [Sentry golang docs](https://docs.sentry.io/clients/go)
+
+```go
+package main
+
+import "github.com/getsentry/raven-go"
+
+func init() {
+    // Setup sentry
+    raven.SetDSN("https://<key>:<secret>@sentry.io/<project>")
+    
+    // Setup cronjobs ...
+}
+```
---
+++ helper.go
@@ -0,0 +1,114 @@
+package helper
+
+import (
+	"os"
+	"log"
+	"time"
+	"strconv"
+	"encoding/json"
+	"math/rand"
+	"crypto/x509"
+	"io/ioutil"
+	"path/filepath"
+	"errors"
+	"github.com/getsentry/raven-go"
+)
+
+func FloatToString(float float64) string {
+	return strconv.FormatFloat(float, 'f', 6, 64)
+}
+
+func StringToFloat(string string, bitSize int) float64 {
+	float, err := strconv.ParseFloat(string, bitSize)
+
+	if err != nil {
+		raven.CaptureError(err, nil)
+		log.Fatal(err.Error())
+	}
+
+	return float
+}
+
+func TimestampStringToDate(timestamp string) time.Time {
+	timestampInt, err := strconv.ParseInt(timestamp, 10, 64)
+
+	if err != nil {
+		raven.CaptureError(err, nil)
+		log.Fatal(err.Error())
+	}
+
+	return time.Unix(0, timestampInt*int64(time.Millisecond)).UTC()
+}
+
+func Debug(data ... interface{}) {
+	for _, v := range data {
+		log.Printf("[%v] %+v\n", rand.Intn(1000), v)
+	}
+
+	log.Print("\n")
+}
+
+func Dump(data interface{}) {
+	log.Printf("%+v\n", data)
+}
+
+func DD(data interface{}) {
+	Dump(data)
+	os.Exit(1)
+}
+
+func ToJSON(data interface{}) string {
+	JSON, err := json.Marshal(data)
+
+	if err != nil {
+		raven.CaptureError(err, nil)
+		log.Fatal(err.Error())
+	}
+
+	return string(JSON)
+}
+
+func Sleep(seconds time.Duration) {
+	time.Sleep(seconds * time.Second)
+}
+
+func CurrentMinute() int64 {
+	return time.Now().Unix() / 60
+}
+
+func ThrowError(message string) {
+	err := errors.New(message)
+	raven.CaptureError(err, nil)
+	log.Fatal(err.Error())
+}
+
+func IntToString(value int) string {
+	return strconv.Itoa(value)
+}
+
+func CertPool(certs []string) *x509.CertPool {
+	roots := x509.NewCertPool()
+
+	for _, cert := range certs {
+		cert, err := ioutil.ReadFile(cert)
+
+		if err != nil {
+			raven.CaptureError(err, nil)
+			log.Fatal(err.Error())
+		}
+
+		roots.AppendCertsFromPEM(cert)
+	}
+
+	return roots
+}
+
+func LoadFile(file string) string {
+	callPath := filepath.Dir(os.Args[0])
+
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return callPath + "/" + file
+	}
+
+	return file
+}
